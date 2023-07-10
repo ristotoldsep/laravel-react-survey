6fe854c0eecdfc80b895b041c5ceca8408500aee
Risto Tõldsep <risto@MacBook-Pro.local>
2023-07-10 16:26:29 +0300

    Created basic layouts and configured routes

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,33 +1,44 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
 import Dashboard from "./views/Dashboard";
-import Surveys from "./views/Surveys";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
-import GuestLayout from "./components/GuestLayout";
+import Surveys from "./views/Surveys";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Dashboard />
-    },
-    {
-        path: '/surveys',
-        element: <Surveys />
-    },
-    {
-        path: '/',
-        element: <GuestLayout />,
-        children: [
-            {
-                path: '/login',
-                element: <Login />
-            },
-            {
-                path: '/signup',
-                element: <Signup />
-            },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/dashboard',
+        element: <Navigate to="/" />
+      },
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/surveys",
+        element: <Surveys />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 export default router;
--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
+import { EyeIcon, PencilIcon } from "@heroicons/react/24/outline";
 
 export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+
+ 
+
   return (
-    <div>Dashboard</div>
-  )
+    <>
+    Dashboard
+    </>
+  );
 }
